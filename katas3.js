5bcb19304b99c6a166f2c2c270cb7c022bdb3a8d
Arthur Sarmento <arthurbs17@gmail.com>
2021-07-13 12:03:16 -0300

    Tentando resolver katas bonus

--- katas3.js
+++ katas3.js
@@ -297,10 +297,40 @@ function kataBonus1() {
 
 function kataBonus2() {
     // implemente o código do kata bonus 2 aqui
+    let boxes = document.querySelectorAll('#box > div > ul > li:nth-child(n)');
+    let boxArrays = [...boxes];
+    let boxSizes = [];
+    let firstSize = 100
+
+    for (let i = 0; i < boxArrays.length; i++){
+        firstSize += 5
+        boxSizes.push(firstSize);
+    }
+
+    for (let i = 0; i < boxArrays.length; i++){
+        boxArrays[i].style.width = boxSizes[i].toString()+'px'
+    }
+    
+    showBox(boxArrays)
 }
 
 function kataBonus3() {
     // implemente o código do kata bonus 3 aqui
+    let boxes = document.querySelectorAll('#box > div > ul > li:nth-child(n)');
+    let boxArrays = [...boxes];
+    let boxSizes = [];
+    let firstSize = 100
+
+    for (let i = 0; i < boxArrays.length; i++){
+        firstSize += 5
+        boxSizes.push(firstSize);
+    }
+
+    for (let i = 0; i < boxArrays.length; i++){
+        boxArrays[i].style.width = sampleArray[i].toString()+'px'
+    }
+
+    showBox(boxArrays)
 }
 
 function kataBonus4() {
@@ -332,3 +362,4 @@ kata18();
 
 kataBonus1();
 kataBonus2();
+kataBonus3();
